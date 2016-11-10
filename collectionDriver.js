7e8d5b44e808def252845d20b2f7bc31f896c7b6
mverdier <mart.verdier@gmail.com>
2016-11-10 16:23:59 +0100

    Linked mongo driver to main API

--- collectionDriver.js
+++ collectionDriver.js
@@ -13,41 +13,17 @@ CollectionDriver.prototype.getCollection = function(collectionName, callback) {
 };
 
 //Finds objects according to a criteria in the DB
-CollectionDriver.prototype.get = function(collectionName, name, brand, country, taste, flavor, color, callback) {
+CollectionDriver.prototype.get = function(collectionName, filters, callback) {
 	this.getCollection(collectionName, function(error, the_collection) {
 		if (error) callback(error);
 		else {
-
-			var filters = {};
-
-			if (name !== undefined && name !== "") {
-				filters.name = name;
-			}
-
-			if (brand !== undefined && brand !== "") {
-				filters.brand = brand;
-			}
-
-			if (country !== undefined && country !== "") {
-				filters.country = country;
-			}
-
-			if (taste !== undefined && taste !== "") {
-				filters.taste = taste;
-			}
-
-			if (flavor !== undefined && flavor !== "") {
-				filters.flavor = flavor;
-			}
-
-			if (color !== undefined && color !== "") {
-				filters.color = color;
-			}
-
-			the_collection.find({filters}, function(error, doc) {
+			//Finding data in the DB
+			the_collection.find(filters, function(error, doc) {
 				if (error) callback(error);
 				else callback(null, doc);
 			});
 		}
 	});
 };
+
+exports.CollectionDriver = CollectionDriver;
--- waiter.js
+++ waiter.js
@@ -3,15 +3,43 @@ var http = require('http'),
 	express = require('express'),
 	path = require('path');
 
+MongoClient = require('mongodb').MongoClient,
+Server = require('mongodb').Server,
+CollectionDriver = require('./collectionDriver').CollectionDriver;
+
 var app = express();
 app.set('port', process.env.PORT || 3000);
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/get', function (req, res) {
-	res.send('{"name": ' + req.query.name + ', "brand": ' + req.query.brand + ', "country": ' + req.query.country + ', "taste": ' + req.query.taste + ', }');
+var mongoHost = 'localHost';
+var mongoPort = 27017; 
+var collectionDriver;
+ 
+//Starting the collection driver linked to our mongo database
+var mongoClient = new MongoClient(new Server(mongoHost, mongoPort));
+mongoClient.open(function(err, mongoClient) {
+	if (!mongoClient) {
+		console.error("Error! Exiting... Must start MongoDB first");
+		process.exit(1);
+	}
+
+	var db = mongoClient.db("beer");
+
+	collectionDriver = new CollectionDriver(db);
 });
 
-//Creating a web server
-http.createServer(app).listen(app.get('port'), function(){
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Getting a list of beers according to the required filters
+app.get('/get', function (req, res) {
+	
+	//Fetching data
+	collectionDriver.get("beer", req.query, function(error, objs) {
+		if (error) { res.send(400, error); }
+		else { res.send(200, objs); }
+	});
+});
+
+//Creating the web server
+http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
 });
