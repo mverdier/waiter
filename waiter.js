b1b124b82a55a0f91d2e43129403852e4399e6de
mverdier <mart.verdier@gmail.com>
2016-11-10 14:23:14 +0100

    Added packages and initiated http server

--- waiter.js
+++ waiter.js
@@ -1,1 +1,10 @@
-console.log("HELLO WORLD");
+//Using HTTP libraries
+var http = require('http');
+ 
+//Creating a web server
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end('{"response": "OK"}');
+}).listen(3000);
+ 
+console.log('Server running on port 3000.');
